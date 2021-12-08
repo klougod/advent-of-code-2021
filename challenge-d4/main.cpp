7dd156cfce6793ac889bc67a99add8f2182713fa
klougod <brunoomagalhaes98@gmail.com>
2021-12-08 16:34:26 -0300

    refac: folders

--- challenge-d4/main.cpp
+++ challenge-d4/main.cpp
@@ -18,6 +18,10 @@ struct bingo {
 
 vector<vector<bingo_entry>> empty_table(5, vector<bingo_entry>(5, {value: 0, checked: false}));
 
+void printRed(int str) {
+  printf("\33[38;2;255;0;0m %2d \33[m", str);
+};
+
 void populate_bingo_file(ifstream *file, struct bingo *bingo) {
   string input;
   regex space_reg("\\s+");
@@ -64,9 +68,7 @@ void populate_bingo_file(ifstream *file, struct bingo *bingo) {
 
 }
 
-// TODO: find winner value
-int check_winner(struct bingo *bingo, int value_to_find) {
-  // check input with entry values
+int check_winner_part1(struct bingo *bingo, int value_to_find) {
   for(int i=0; i < bingo->tables.size(); ++i) {
     for(int j=0; j < bingo->tables[i].size(); ++j) {
       for(int k=0; k < bingo->tables[i][j].size(); ++k) {
@@ -93,7 +95,6 @@ int check_winner(struct bingo *bingo, int value_to_find) {
         }
       }
       if(found_row) {
-        cout << "FOUND COL: " << " table: " << i << " index: " << j << endl << endl;
         int sum_of_unchecked = 0;
         for(int l=0; l < bingo->tables[i].size(); ++l) {
           for(int m=0; m < bingo->tables[i][l].size(); ++m) {
@@ -106,7 +107,6 @@ int check_winner(struct bingo *bingo, int value_to_find) {
       }
 
       if(found_col) {
-        cout << "FOUND ROW: " << " table: " << i << " index: " << j << endl << endl;
         int sum_of_unchecked = 0;
         for(int l=0; l < bingo->tables[i].size(); ++l) {
           for(int m=0; m < bingo->tables[i][l].size(); ++m) {
@@ -122,6 +122,72 @@ int check_winner(struct bingo *bingo, int value_to_find) {
   return -1;
 }
 
+
+int check_winner_part2(struct bingo *bingo, int value_to_find) {
+  for(int i=0; i < bingo->tables.size(); ++i) {
+    for(int j=0; j < bingo->tables[i].size(); ++j) {
+      for(int k=0; k < bingo->tables[i][j].size(); ++k) {
+        if(bingo->tables[i][j][k].value == value_to_find) {
+          bingo->tables[i][j][k].checked = true;
+        }
+      }
+    }
+  }
+
+  int sum_of_unchecked = 0;
+  int found_cell = 0;
+
+  int i,j,k;
+
+  int checked_k = -1;
+  for(i=0; i < bingo->tables.size(); ++i) {
+    for(j=0; j < bingo->tables[i].size(); ++j) {
+      bool found_row = true;
+      bool found_col = true;
+
+      for(k=0; k < bingo->tables[i][j].size(); ++k) {
+        if(!bingo->tables[i][k][j].checked) {
+          found_row = false;
+        } else {
+          checked_k = k;
+        }
+
+        if(!bingo->tables[i][j][k].checked) {
+          found_col = false;
+        } else {
+          checked_k = k;
+        }
+      }
+      if(found_row) {
+        found_cell = bingo->tables[i][j][checked_k].value;
+        sum_of_unchecked = 0;
+        for(int l=0; l < bingo->tables[i].size(); ++l) {
+          for(int m=0; m < bingo->tables[i][l].size(); ++m) {
+            if(bingo->tables[i][l][m].checked == false) {
+              sum_of_unchecked += bingo->tables[i][l][m].value;
+            }
+          }
+        }
+      }
+
+      if(found_col) {
+        found_cell = bingo->tables[i][j][checked_k].value;
+        sum_of_unchecked = 0;
+
+        for(int l=0; l < bingo->tables[i].size(); ++l) {
+          for(int m=0; m < bingo->tables[i][l].size(); ++m) {
+            if(bingo->tables[i][l][m].checked == false) {
+              sum_of_unchecked += bingo->tables[i][l][m].value;
+            }
+          }
+        }
+      }
+    }
+    cout << "FOUND_NUMBER_" << i << '\n';
+  }
+  return sum_of_unchecked * found_cell;
+}
+
 void print_bingo(struct bingo *bingo) {
   for(int i=0; i < bingo->entry.size(); ++i) {
     cout << bingo->entry[i] << ',';
@@ -132,7 +198,11 @@ void print_bingo(struct bingo *bingo) {
   for(int i=0; i < bingo->tables.size(); ++i) {
     for(int j=0; j < bingo->tables[i].size(); ++j) {
       for(int k=0; k < bingo->tables[i][j].size(); ++k) {
-        cout << '[' << bingo->tables[i][j][k].value << " " << bingo->tables[i][j][k].checked << "]";
+        if(bingo->tables[i][j][k].checked) {
+          printRed(bingo->tables[i][j][k].value);
+        } else {
+          printf(" %2d ", bingo->tables[i][j][k].value);
+        }
       }
       cout << endl;
     }
@@ -148,10 +218,18 @@ int main(int argc, char *argv[]) {
 
   for(int i=0; i < bingogame.entry.size(); ++i) {
     int value_to_find = bingogame.entry[i];
-    int winner = check_winner(&bingogame, value_to_find);
+    int winner = check_winner_part1(&bingogame, value_to_find);
     if(winner > 0) {
-      cout << "\n WINNER score is: " << winner * value_to_find << endl;
+      cout << "\n WINNER PART1 score is: " << winner * value_to_find << endl;
       break;
+    }
+  }
+
+  for(int i=0; i < bingogame.entry.size(); ++i) {
+    int value_to_find = bingogame.entry[i];
+    int winner = check_winner_part2(&bingogame, value_to_find);
+    if(winner > 0) {
+      cout << "\n WINNER PART2 score is: " << winner << endl;
     }
   }
 
